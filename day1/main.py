8c6e6579581ccf28f8549b38d13036e08bfee997
alishahwee <alisha.hwee@outlook.com>
2022-12-03 19:22:08 -0600

    Complete part 2
    Co-authored-by: scottzyang <scottzyang@gmail.com>

--- day1/main.py
+++ day1/main.py
@@ -3,17 +3,50 @@ input = ''
 with open('input.txt') as f:
     input = f.read()
 
-def find_most_calories(input):
-    calorie_groups = input.split('\n\n')
+
+calorie_groups = input.split('\n\n')
+
+
+def calculate_total(i):
+    """Calculate the total cals for a group by given index"""
+    calories = calorie_groups[i].split('\n')
+    total = 0
+    for calorie in calories:
+        total += int(calorie)
+
+    return total
+
+
+def find_most_calories(calorie_groups, skipped_groups):
+    """Return the index of the most calories in group"""
     most_calories = 0
-    for calorie_group in calorie_groups:
-        calories = calorie_group.split('\n')
-        total = 0
-        for calorie in calories:
-            total += int(calorie)
+    group_index = None
+
+    for i in range(len(calorie_groups)):
+        if i in skipped_groups:
+            continue
+
+        total = calculate_total(i)
         if total > most_calories:
             most_calories = total
-    return most_calories
+            group_index = i
+
+    return group_index
 
 
-print(find_most_calories(input))
+def top_calories(n):
+    """Return the total calories of top n groups"""
+    top_groups = set()
+    for i in range(n):
+        group = find_most_calories(calorie_groups, top_groups)
+        top_groups.add(group)
+
+    total = 0
+    for group in top_groups:
+        group_total = calculate_total(group)
+        total += group_total
+    return total
+
+
+print(f'Part1: {top_calories(1)}')
+print(f'Part2: {top_calories(3)}')
